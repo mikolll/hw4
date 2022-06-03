f6e7aa806e5ee341f66a6b19b69e9eb0069a8857
mikolll <michael.manzano@kellogg.northwestern.edu>
2022-06-03 21:08:05 +0000

    save1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # authenticate the user
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Hello, #{@user["first_name"]}."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Sorry, that is wrong."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Sorry, that is wrong."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
